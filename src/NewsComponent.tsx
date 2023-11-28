ad95bc5dfec801207dcfd80b74117a80a3c31892
AFS0027473 <AFS0027473@agderskolen.no>
2023-11-28 10:02:31 +0100

    made news component

---
+++ src/NewsComponent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+const News = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+
+export default News;

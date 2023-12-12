168fb03d1c20a9ed4d1c297f94c09f9ffd24b30b
HermanErKu <herman.lindalen.nodeland@outlook.com>
2023-12-12 14:06:51 +0100

    fixed itslearning rss component

--- src/ItsLearningComponent.tsx
+++ src/ItsLearningComponent.tsx
@@ -1,29 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import RSSParser from 'react-native-rss-parser';
-
+import * as Linking from 'expo-linking';
+import * as WebBrowser from 'expo-web-browser';
 
 const ItsLearning = () => {
-    const [rssData, setRssData] = useState("");
-
+    const [rssData, setRssData] = useState([]);
+ 
     const rssUrl = process.env.EXPO_PUBLIC_ITSLEARNING_LINK; 
-
+ 
     useEffect(() => {
         fetch(rssUrl)
             .then(response => response.text())
             .then(responseData => RSSParser.parse(responseData))
             .then(rss => {
-                setRssData(rss)
+                if (Array.isArray(rss.items)) {
+                   setRssData(rss.items);
+                }
             });
-    }, []);
-     
-
-    return (
-        <View>
-            <Text style={{fontSize: 16}}>{rssData["title"]}</Text>
+    }, []);   
+    
+    const map = [0, 1, 2];
+    const elements = map.map((item, index) => (
+        <View key={item}>
+            <Text style={{fontSize: 20}}>{rssData && rssData[index] && rssData[index].title}</Text>
+            {/*<Text>{rssData && rssData[index] && rssData[index].links[0].url} {'\n'}</Text>*/}
+            <TouchableOpacity onPress={() => WebBrowser.openBrowserAsync(rssData && rssData[index] && rssData[index].links[0].url)}>
+               <Text>{rssData && rssData[index] && rssData[index].links[0].url} {'\n'}</Text>
+           </TouchableOpacity>
         </View>
+    ));
+     
+ 
+    return (
+        <ScrollView>
+            {elements}
+        </ScrollView>
     )
-}
-
-export default ItsLearning;
+ }
+ 
+ export default ItsLearning;

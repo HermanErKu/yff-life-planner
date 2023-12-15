aa984eeacbeaa1859bf16003665a575c841867f0
HermanErKu <herman.lindalen.nodeland@outlook.com>
2023-12-15 12:50:15 +0100

    Made small changes to travel component. Going to finish later

---
+++ src/TravelComponent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, ScrollView } from "react-native";
+
+
+const Travel = () => {
+    return (
+        <ScrollView>
+            <Text>travel component</Text>
+        </ScrollView>
+    );
+};
+
+
+export default Travel;

b78104ffe21fb11f4c5ecf50ee4a0e9ddac07438
HermanErKu <herman.lindalen.nodeland@outlook.com>
2023-12-15 08:57:43 +0100

    added refresh app button in header

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import HomeScreen from './HomeScreen';
 import SettingsScreen from './SettingsScreen';
 
 import { Ionicons } from '@expo/vector-icons';
+import * as Updates from 'expo-updates';
+
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,14 +20,22 @@ const MainNavigator = () => (
 );
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: ({ focused }) => (<Ionicons name={focused ? 'home' : 'home-outline'} size={26} style={{ marginBottom: -3 }}/>),}}/>
-        <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon: ({ focused }) => (<Ionicons name={focused ? 'settings' : 'settings-outline'} size={26} style={{ marginBottom: -3 }}/>),}}/>
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+    const refreshApp = async () => {
+        try {
+            await Updates.reloadAsync();
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: ({ focused }) => (<Ionicons name={focused ? 'home' : 'home-outline'} size={26} style={{ marginBottom: -3 }}/>), headerLeft: () => ( <Ionicons onPress={() => refreshApp()} name={'refresh'} size={26} style={{ marginBottom: -3, marginLeft: 10, }}/> )}}/>
+                <Tab.Screen name="Settings" component={SettingsScreen} options={{tabBarIcon: ({ focused }) => (<Ionicons name={focused ? 'settings' : 'settings-outline'} size={26} style={{ marginBottom: -3 }}/>),}}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default App;

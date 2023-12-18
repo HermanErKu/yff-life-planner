795be391b24056f8f7662716570eda45a5dc3b7a
HermanErKu <herman.lindalen.nodeland@outlook.com>
2023-12-18 08:51:06 +0100

    did small changes, made scrollable page, added more settings

--- SettingsScreen.tsx
+++ SettingsScreen.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, TextInput, Button, Switch } from 'react-native';
+import { View, StyleSheet, Text, TextInput, Button, Switch, ScrollView } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import TestComponent from '../yff-life-planner/src/testComponent';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -83,80 +83,89 @@ const SecondScreen = () => {
     updateSwitchesFromSave();
 
     return (
-        <View style={styles.container}>
-            {/* Buss / bil switch */}
-            <Text>{'\n'}Tar du buss:{'\n'}</Text>
-            <Switch
-                trackColor={{false: '#767577', true: '#81b0ff'}}
-                thumbColor={isBussing ? '#f5dd4b' : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleIsBussingSwitch}
-                value={isBussing}
-            />
-            <Text>{'\n'}{isBussing.toString()}</Text>
+        <ScrollView>
+            <View style={styles.container}>
+                {/* Buss / bil switch */}
+                <Text>{'\n'}Tar du buss:{'\n'}</Text>
+                <Switch
+                    trackColor={{false: '#767577', true: '#81b0ff'}}
+                    thumbColor={isBussing ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleIsBussingSwitch}
+                    value={isBussing}
+                />
+                <Text>{'\n'}{isBussing.toString()}</Text>
 
-            
-            <View style={styles.line} />
-            {/*  */}
-            <Text style={{textAlign:'center'}}>Hvilket busstopp tar du buss fra?{'\n'}</Text>
-            <TextInput
-                style={{ paddingTop:20, height: 40, width:"90%", alignSelf:"center", borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={text => updateBusstopp(text)}
-            />
-            {data ? <Text>{data['suggestions']}</Text> : <Text>Test</Text>}
-            <View style={styles.line} />
-            
+                
+                {/* Hvilket busstopp (er ikke ferdig med api enda) */}
+                {/*
+                <Text style={{textAlign:'center'}}>Hvilket busstopp tar du buss fra?{'\n'}</Text>
+                <TextInput
+                    style={{ paddingTop:20, height: 40, width:"90%", alignSelf:"center", borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={text => updateBusstopp(text)}
+                />
+                {data ? <Text>{data['suggestions']}</Text> : <Text>Test</Text>}
+                
+                <View style={styles.line} />
+                */}
 
-            <View style={styles.line} />
-            {/* In app browser switch */}
-            <Text>{'\n'}In app browser:{'\n'}</Text>
-            <Switch
-                trackColor={{false: '#767577', true: '#81b0ff'}}
-                thumbColor={inAppBrowser ? '#f5dd4b' : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleBrowserSwitch}
-                value={inAppBrowser}
-            />
-            <Text>{'\n'}{inAppBrowser.toString()}</Text>
+                {/* What bus switch if bussing */}
+                {isBussing ? <Text style={{textAlign:'center'}}>{'\n\n'}Hvilken buss tar du? (eks: M1, 40){'\n'}</Text> : null}
+                {isBussing ? <TextInput style={{ paddingTop:20, height: 40, width:"90%", alignSelf:"center", borderColor: 'gray', borderWidth: 1 }} onChangeText={async text => await AsyncStorage.setItem('busLineStore', text)}/> : null}
+                
 
-            
-            {/* Layout Settings */}
-            <View style={styles.line} />
-            {/* Travel switch */}
-            <Text>{'\n'}Travel updates:{'\n'}</Text>
-            <Switch
-                trackColor={{false: '#767577', true: '#81b0ff'}}
-                thumbColor={travel ? '#f5dd4b' : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleTravelSwitch}
-                value={travel}
-            />
-            <Text>{'\n'}{travel.toString()}</Text>
+                <View style={styles.line} />
+                {/* In app browser switch */}
+                <Text>In app browser:{'\n'}</Text>
+                <Switch
 
+                    trackColor={{false: '#767577', true: '#81b0ff'}}
+                    thumbColor={inAppBrowser ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleBrowserSwitch}
+                    value={inAppBrowser}
+                />
+                <Text>{'\n'}{inAppBrowser.toString()}</Text>
 
-            {/* News switch */}
-            <Text>{'\n'}News updates:{'\n'}</Text>
-            <Switch
-                trackColor={{false: '#767577', true: '#81b0ff'}}
-                thumbColor={news ? '#f5dd4b' : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleNewsSwitch}
-                value={news}
-            />
-            <Text>{'\n'}{news.toString()}</Text>
+                
+                {/* Layout Settings */}
+                <View style={styles.line} />
+                {/* Travel switch */}
+                <Text>{'\n'}Travel updates:{'\n'}</Text>
+                <Switch
+                    trackColor={{false: '#767577', true: '#81b0ff'}}
+                    thumbColor={travel ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleTravelSwitch}
+                    value={travel}
+                />
+                <Text>{'\n'}{travel.toString()}</Text>
 
 
-            {/* ItsLearning switch */}
-            <Text>{'\n'}ItsLearning updates:{'\n'}</Text>
-            <Switch
-                trackColor={{false: '#767577', true: '#81b0ff'}}
-                thumbColor={itslearning ? '#f5dd4b' : '#f4f3f4'}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleItslearningSwitch}
-                value={itslearning}
-            />
-            <Text>{'\n'}{itslearning.toString()}</Text>
-        </View>
+                {/* News switch */}
+                <Text>{'\n'}News updates:{'\n'}</Text>
+                <Switch
+                    trackColor={{false: '#767577', true: '#81b0ff'}}
+                    thumbColor={news ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleNewsSwitch}
+                    value={news}
+                />
+                <Text>{'\n'}{news.toString()}</Text>
+
+
+                {/* ItsLearning switch */}
+                <Text>{'\n'}ItsLearning updates:{'\n'}</Text>
+                <Switch
+                    trackColor={{false: '#767577', true: '#81b0ff'}}
+                    thumbColor={itslearning ? '#f5dd4b' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleItslearningSwitch}
+                    value={itslearning}
+                />
+                <Text>{'\n'}{itslearning.toString()}</Text>
+            </View>
+        </ScrollView>
   );
 };
 

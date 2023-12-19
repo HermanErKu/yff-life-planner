3eb006ea3bff74e348c01e9ba298ab1cfa7500b8
HermanErKu <herman.lindalen.nodeland@outlook.com>
2023-12-19 11:02:49 +0100

    did small changes to bus layout

--- src/TravelComponent.tsx
+++ src/TravelComponent.tsx
@@ -51,7 +51,7 @@ const Travel = () => {
 
     return (
         <ScrollView>
-            {isBussing ? data.map((item, index) => { const key = Object.keys(item)[0]; const destination = item[key].destination; const lineName = item[key].lineName; const occupancy = item[key].occupancy; const delay = item[key].delay; const currentStop = item[key].currentStop; if (delay / 60! > -7 && delay / 60! < 15) { return (<Text key={index}>{lineName} - {destination}.  {occupancy} {'\n'} {Math.round(delay / 60)} Minutter forsinket. På: {currentStop} {'\n'}</Text>); } }) : <Text>You are drivig</Text>}
+            {isBussing ? data.map((item, index) => { let occupancyNumber = ""; let occupancyText = ""; const key = Object.keys(item)[0]; const destination = item[key].destination; const lineName = item[key].lineName; const occupancy = item[key].occupancy; const delay = item[key].delay; const currentStop = item[key].currentStop; if (occupancy == "ManySeatsAvailable") { occupancyNumber = "1/3"; occupancyText = "Mange sitteplasser tilgjengelig"; } else if (occupancy == "SeatsAvailable") { occupancyNumber = "2/3"; occupancyText = "Noen sitteplasser tilgjengelig"; } else { occupancyNumber = "3/3"; occupancyText = "Ståplasser tilgjengelig"; };  if (delay / 60! > -3 && delay / 60! < 15) { return (<Text key={index}>{lineName} - {destination}.{'\n'}{occupancyText} - {occupancyNumber}{'\n'}{Math.round(delay / 60)} Minutter forsinket{'\n'}På: {currentStop} {'\n'}</Text>); } }) : <Text>You are drivig</Text>}
         </ScrollView>
         
     );
